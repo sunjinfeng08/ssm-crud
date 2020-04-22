f27b6f337e211628ce8baca87f8bdb75699b4982
sunjinfeng08 <sunjinfeng08@163.com>
2020-04-22 13:35:52 +0800

    SSM-Integration

---
+++ src/main/java/com/atguigu/crud/controller/DepartmentController.java
@@ -0,0 +1,41 @@
+package com.atguigu.crud.controller;
+
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.atguigu.crud.bean.Department;
+import com.atguigu.crud.bean.Msg;
+import com.atguigu.crud.service.DepartmentService;
+import com.atguigu.crud.test.LogTest;
+
+/**
+ * 处理和部门有关的请求
+ * @author lfy
+ *
+ */
+@Controller
+public class DepartmentController {
+	
+	@Autowired
+	private DepartmentService departmentService;
+	
+	/**
+	 * 返回所有的部门信息
+	 */
+	@RequestMapping("/depts")
+	@ResponseBody
+	public Msg getDepts(){
+		//查出的所有部门信息
+		Logger log = LoggerFactory.getLogger(DepartmentController.class);
+		List<Department> list = departmentService.getDepts();
+		log.debug(list.toString());
+		return Msg.success().add("depts", list);
+	}
+
+}
---
+++ src/main/java/com/atguigu/crud/test/LogTest.java
@@ -0,0 +1,19 @@
+package com.atguigu.crud.test;
+
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class LogTest {
+	@Test
+	public void test() {
+		Logger log = LoggerFactory.getLogger(LogTest.class);
+		log.warn("this is message {}", 1);
+		Exception ex = new Exception("this is a message.");
+		log.error("a new exeception", ex);
+		log.trace("trace message.");
+		log.info("info message.");
+		for (int i = 0; i < 2000; i++)
+			log.debug("debug message:{}={}", "line", i);
+	}
+}
